8b44057f6404aed6d975836bbeaff0d3d1ab0bd8
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-05 16:10:21 +0300

    Created Root View Controller in Scene Delegate

---
+++ LoginWithToken/LoginWithToken/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginWithToken
+//
+//  Created by Berat Rıdvan Asiltürk on 5.09.2023.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
