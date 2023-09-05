c53fe77da43eb7639eeb1cc7595491d0f9826a99
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-05 17:42:38 +0300

    Added Mock Login Storage Service | Unit Test

--- LoginWithToken/LoginWithTokenTests/LoginWithTokenTests.swift
+++ LoginWithToken/LoginWithTokenTests/LoginWithTokenTests.swift
@@ -10,27 +10,42 @@ import XCTest
 
 final class LoginWithTokenTests: XCTestCase {
 
+    weak var viewModel : RootViewModel!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // ViewModel'i init ettik
+        viewModel = RootViewModel(loginStorageService: <#T##LoginStorageService#>)
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
-
 }
+
+// Mock clasimiz eklendikten sonra stublari otomatik hatadan cektik
+class MockLoginStorageService : LoginStorageService {
+    
+    var userAccessTokenKey: String {
+        return "ACCESS_TOKEN"
+    }
+    
+    var storage: [String : String] = [:]
+    
+    // UserDefaults'u herhangi bir sozlukle beraber mock yani taklid ediyoruz bu projemizde bu iki func ile
+    func setUserAccessToken(token: String) {
+        // Ona deger olarak verilen token'a userAccessTokenKey'i koyduk
+        storage[userAccessTokenKey] = token
+    }
+    func getUserAccessToken() -> String? {
+        // Istenilen anahtari vererek degeri almaya calisacagiz
+        return storage[userAccessTokenKey]
+    }
+}
+
+
