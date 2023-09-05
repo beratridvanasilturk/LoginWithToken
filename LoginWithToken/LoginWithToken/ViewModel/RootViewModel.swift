b5f0bc5b433f452e977b2202ab1a6b5642b8ef7d
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-05 17:16:34 +0300

    Added LoginStorageService Protoc. in ViewModel & Updated checkLogin Func

--- LoginWithToken/LoginWithToken/ViewModel/RootViewModel.swift
+++ LoginWithToken/LoginWithToken/ViewModel/RootViewModel.swift
@@ -10,15 +10,24 @@ import Foundation
 // MVVM icin Model olusturuldu
 class RootViewModel {
     
+    // LoginStorageService Protokolunu kullanarak ViewModel icerisinde yaptigimiz islemi soyutlamada ve Check Login func'nunda kullandik
+    // Oncelikle LoginStorageService protokolunu getirdik View Modele, init hatasindan sonra init'i de ekleriz
+    private let loginStorageService : LoginStorageService
+    
     // Delegate pattern'de kullanacagimiz icin RootViewModelOutput'u weak var olarak olusturduk
     weak var output: RootViewModelOutput?
      
+    // LoginStorageService eklendikten sonra init'i ekledik
+    init(loginStorageService: LoginStorageService, output: RootViewModelOutput? = nil) {
+        self.loginStorageService = loginStorageService
+        self.output = output
+    }
     
     
     // Endustride User Defaults'ta tokenleri tutmak istemeyiz cunku hem guvenli degildir hem de MVVM yapisina uygun degildir ancak kod olarak bir problemi yoktur is gorecektir ama tokenler'i kontrol etmenin en dogru yolu degildir.
     func checkLogin() {
         
-        if let accessToken = UserDefaults.standard.string(forKey: "ACCESS_TOKEN"), !accessToken.isEmpty {
+        if let accessToken = loginStorageService.getUserAccessToken(), !accessToken.isEmpty {
             // Eger token eslesmesi basarili ise main'e git
 //            showMainVC()
             output?.showMainVC()
