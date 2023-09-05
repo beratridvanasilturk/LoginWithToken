8f226b29d3a15ac1e4b6b5ebb3c9530c6836faeb
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-05 16:45:11 +0300

    Injected ViewModel and Created Init & Required Init in RootVC | Injected

--- LoginWithToken/LoginWithToken/View/RootViewController.swift
+++ LoginWithToken/LoginWithToken/View/RootViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-// RootViewOutput'u delege olarak ayarladiktan sonra View Modelden gelecek output'a gore showMainVC ve showLoginVC funclari calistirilacaktir
+// RootViewOutput'u delege olarak ayarladiktan sonra View Modelden gelecek output'a gore showMainVC ve showLoginVC funclari calistirilacaktir. Bunun icin de View Model'in enjekte etmemiz gereklidir
 class RootViewController: UIViewController, RootViewModelOutput {
 
+    // ViewModel enjekte edildikten sonra init ve required init'i cagrilmasi gereklidir
+    private var viewModel: RootViewModel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .gray
         
     }
+    
+    //
+    init(viewModel: RootViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        // son olarak viewModel'in output'unu self'e esitleriz cunku kendisi artik delege aldigi icin bir rootViewModelOutput'tur
+        viewModel.output = self
+    }
+    
+    // required init'i hata mesajindan otomatik doldurduk
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     func showMainVC() {
         let mainVC = MainViewController()
